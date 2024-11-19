29db2c8c3ac2019cafe96e4ae795f3bf4f219ba9
AC1105AL <clima.ana2006@gmail.com>
2024-11-18 21:30:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes. “Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
+)
+
+criaCartao(
+    'Geografia',
+    'O Brasil é de que continente?',
+    'America do sul'
+)
+
+criaCartao(
+    'Historia',
+    'Quais as duas datas que são comemoradas em novembro?',
+    'Proclamação da República e Dia Nacional da Consciência Negra.A Proclamação da República é comemorada no dia 15 de novembro, data em que Marechal Deodoro da Fonseca proclamou a república em 1889.O Dia Nacional da Consciência Negra é comemorado no dia 20 de novembro, data em que Zumbi dos Palmares morreu, em 1695.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que a palavra legend significa em português?',
+    'Lenda. Legend, que parece significar “legenda”, faz parte do grupo dos falsos cognatos. Falsos cognatos são palavras de línguas distintas que apresentam semelhanças na grafia e/ou na pronúncia, mas cujos significados são diferentes. Outros exemplos são: costume, que significa fantasia, e to push, que significa empurrar.'
+) 
+
+criaCartao(
+    'Quimica',
+    'Quantos ossos temos no nosso corpo?',
+    '(206).O corpo humano tem 206 ossos: 22 na cabeça, 1 no pescoço, 6 no ouvido, 44 no tórax, 7 no abdômen, 62 nos membros inferiores e 64 nos membros inferiores.'
+) 
+
+criaCartao(
+    'Matematica',
+    'Quantas casas decimais tem o número pi?',
+    ' Infinitas. Ao longo dos tempos, vários estudiosos têm se dedicado a calcular o número pi e já chegaram ao número de 62,8 trilhões de casas decimais.'
+) 
+
+criaCartao(
+    'Futebol',
+    'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    '7.Uma partida de futebol conta com o número máximo de 11 jogadores e mínimo de 7, contando com o goleiro, em cada equipe.'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz?',
+    ' 299 792 458 metros por segundo (m/s). A primeira medição real da luz foi feita pelo astrônomo Ole Romer, que em 1676 chegou a um número próximo da velocidade da luz. Hoje, sabe-se com precisão que a velocidade da luz é 299 792 458 metros por segundos'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Como é a conjugação do verbo caber na 1.ª pessoa do singular do presente do indicativo?',
+    'Eu caibo. Caber é um verbo irregular da 2ª conjugação. Apesar de estranho, “eu caibo” é a forma correta de conjugá-lo na 1ª pessoa do presente do indicativo.'
+)
